dc70322161239e06a524faa662ae762b55feb375
Patarb <birolpatar@hotmail.com>
2024-01-19 00:19:38 +0300

    yeni template oluşturuldu

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
+from datetime import *
 from django.shortcuts import render
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=datetime.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts':posts})
